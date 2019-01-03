7698e2eed5944bacae7bb3529547f27dcccbc98d
sotowang <sotowang@qq.com>
2019-01-03 21:58:22 +0800

    在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5

--- src/Solution.java
+++ src/Solution.java
@@ -173,12 +173,40 @@ public class Solution {
 //        System.out.println(arrayList);
     }
 
+    /**
+     * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+     * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
+     */
+    public ListNode deleteDuplication(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return pHead;
+        }
+        if (pHead.val == pHead.next.val) {
+            ListNode pNode = pHead.next;
+            while (pNode != null && pNode.val == pHead.val) {
+                pNode = pNode.next;
+            }
+            return deleteDuplication(pNode);
+        }else {
+            pHead.next = deleteDuplication(pHead.next);
+            return pHead;
+        }
+
+    }
+
+    /**
+     * 一个整型数组里除了两个数字之外，其他的数字都出现了偶数次。请写程序找出这两个只出现一次的数字。
+     */
+    public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+
+    }
 
     /**
      * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
      */
 
     Boolean isBalanced = true;
+
     public boolean IsBalanced_Solution(TreeNode root) {
         getDepth(root);
         return isBalanced;
