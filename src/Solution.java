22c782c618be15414b850199fce31ccb1f132ae1
sotowang <sotowang@qq.com>
2019-02-23 10:07:56 +0800

    给一个数组，返回它的最大连续子序列的和，

--- src/Solution.java
+++ src/Solution.java
@@ -172,9 +172,42 @@ public class Solution {
 //        int[] a = {2,2,2,2,2,1,3,4,5};
 //        System.out.println(MoreThanHalfNum_Solution(a));
 
-        int[] input = {4,5,1,6,2,7,3,8};
-        System.out.println(GetLeastNumbers_Solution(input, 10));
+//        int[] input = {4,5,1,6,2,7,3,8};
+//        System.out.println(GetLeastNumbers_Solution(input, 10));
+        int[] array = {6, -3, -2, 7, -15, 1, 2, 2};
+        int[] array2 = {-2,-8,-1,-5,-9};
+        int[] array3 = {1,-2,3,10,-4,7,2,-5};
+        System.out.println(FindGreatestSumOfSubArray(array));
     }
+
+    /**
+     * HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。
+     * 今天测试组开完会后,他又发话了:在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+     * 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+     * 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+     * 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+     */
+    public static int FindGreatestSumOfSubArray(int[] array) {
+            if (array.length == 0) {
+                return 0;
+            }
+            int max = array[0];
+            int sum = array[0];
+            for (int i = 1; i < array.length; i++) {
+                sum += array[i];
+                if (sum < 0) {
+                    sum = array[++i];
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+
+            return max;
+
+
+    }
+
 
     /**
      * 输入n个整数，找出其中最小的K个数。
