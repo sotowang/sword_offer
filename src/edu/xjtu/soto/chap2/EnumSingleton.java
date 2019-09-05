533eda8525cb80a0a4551c17d1a9f3b6fa8a96fe
sotowang <sotowang@qq.com>
2019-09-05 20:20:40 +0800

    Enum

---
+++ src/edu/xjtu/soto/chap2/EnumSingleton.java
@@ -0,0 +1,9 @@
+package edu.xjtu.soto.chap2;
+
+/**
+ * @author wst
+ * @create 2019-09-05 20:18
+ */
+public enum EnumSingleton {
+    INSTANCE;
+}
--- src/edu/xjtu/soto/chap2/LazySingleton.java
+++ src/edu/xjtu/soto/chap2/LazySingleton.java
@@ -5,17 +5,17 @@ package edu.xjtu.soto.chap2;
  */
 public class LazySingleton {
     private LazySingleton() {
-
     }
 
-    private static LazySingleton instance = null;
+    private static volatile LazySingleton instance;
 
+    //DCL
     public static LazySingleton getInstance() {
-        if (instance == null) {
+        if (null == instance) {
             //锁定代码块
             synchronized (LazySingleton.class) {
                 //第二重判断
-                if (instance == null) {
+                if (null == instance) {
                     instance = new LazySingleton();
                 }
             }
